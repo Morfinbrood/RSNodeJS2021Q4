963dea9e21ca0102aa0dc8e67c78e8ffee569045
Yury Staravoitau <yurystaravoitau@gmail.com>
2021-11-11 15:01:23 +0300

    refactor: separeted project

---
+++ config.js
@@ -0,0 +1,8 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const json = require('./assets/alphabets.json');
+
+export const ALPHABET = json.en;
+export const CAESAR_SHIFT = 1;
+export const ROT8_SHIFT = 8;
+
